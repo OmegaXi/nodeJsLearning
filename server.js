8dcc91ee9e82174abb77677a27a78594325f79ad
omegaxi <omegaxi96@outlook.com>
2018-09-20 15:18:46 +0800

    url&router

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ function start(route, handle){
         route(pathname);
 
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write("Hello World\n");
+        var content = route(handle, pathname);
+        response.write(content);
         response.end();
     }
 
