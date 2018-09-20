47eb3ed5e4f21c9956fd2cf7549b79e877b5d9a9
omegaxi <omegaxi96@outlook.com>
2018-09-20 16:26:30 +0800

    notBlock

--- server.js
+++ server.js
@@ -3,20 +3,16 @@ var url = require("url");
 
 
 function start(route, handle){
-    function onRequest(request, response){
+    function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(pathname);
-
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        route(handle, pathname, response);
     }
 
     http.createServer(onRequest).listen(8888);
     console.log("Server starts at http://localhost:8888/");
-}
+    }
+
 
 exports.start = start;
