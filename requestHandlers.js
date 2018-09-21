c25501e5775fe1df909c4429e6b602fb50196b25
omegaxi <omegaxi96@outlook.com>
2018-09-21 13:16:49 +0800

    post

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,23 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
     console.log("Request handler 'start' was called.");
 
-    exec("ls -lah", function (error, stdout, stderr) {
+    var body =
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>';
+
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
+        response.write(body);
         response.end();
-    });
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent the text:" + querystring.parse(postData).text);
     response.end();
 }
 
